122da0c31f287d1a3964aa2ef8991bd7be948dae
civeng <454422353@qq.com>
2020-01-01 22:34:12 +0800

    2020.1.1

---
+++ src/main/java/com/zju/conplat/bean/Node.java
@@ -0,0 +1,12 @@
+package com.zju.conplat.bean;
+
+import java.io.Serializable;
+
+/**
+ *Node节点的监控信息
+ * @author civeng
+ */
+public class Node implements Serializable {
+    private int CPU;
+    private int Mem;
+}
