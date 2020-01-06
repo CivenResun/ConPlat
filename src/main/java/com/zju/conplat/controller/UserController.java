7ce78ef53b9a989f4c4edab55798ef32a5cc9587
civeng <454422353@qq.com>
2020-01-06 22:32:10 +0800

    1.6

--- src/main/java/com/zju/conplat/controller/UserController.java
+++ src/main/java/com/zju/conplat/controller/UserController.java
@@ -1,6 +1,15 @@
 package com.zju.conplat.controller;
 
+import com.zju.conplat.utils.BaseHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -8,5 +17,37 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class UserController {
+
+
+    /**
+     *
+     * todo:指定HashMap大小
+     */
+    @RequestMapping("/cpu")
+    @ResponseBody
+    public Map getCpuUsage(){
+        Map<String,Double> mp=new HashMap<>();
+        for(Pod pod: podList.getItems()){
+            String query="";
+        }
+    }
+
+    /**
+     * 测试方法，没有意义
+     */
+    @RequestMapping("/testJSON")
+    @ResponseBody
+    public String testJson(){
+        BaseHttpClient baseHttpClient=new BaseHttpClient();
+        JSONObject obj= baseHttpClient.getJSON();
+        String conVer=obj.toString();
+        try {
+            JSONObject objTmp=new JSONObject(conVer);
+            System.out.println(objTmp.getJSONArray("value"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return obj.toString();
+    }
 
 }
---
+++ src/test/java/com/zju/conplat/utils/BaseHttpClientTest.java
@@ -0,0 +1,14 @@
+package com.zju.conplat.utils;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.util.TreeSet;
+
+class BaseHttpClientTest {
+    BaseHttpClient baseHttpClient=new BaseHttpClient();
+    @Test
+    void getRes() {
+
+    }
+}
